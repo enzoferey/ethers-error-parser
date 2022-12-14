ec73d65dd53d905425f203a3ce1f4ee0fe634e66
Clément Hallet <clement@challet.eu>
2022-12-14 15:31:33 +0100

    export types from the index file

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,6 @@
 export { getParsedEthersError } from "./getParsedEthersError";
 export { RETURN_VALUE_ERROR_CODES } from "./constants";
+export type { BigNumber } from "./types";
+export type { EthersError } from "./types";
+export type { NestedEthersError } from "./types";
+export type { ReturnValue } from "./types";
