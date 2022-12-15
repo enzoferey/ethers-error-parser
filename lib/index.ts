d9de0f1ec091ce25df0da4387c3cb2eac764ff05
Clément Hallet <clement@challet.eu>
2022-12-15 17:46:02 +0100

    Don't export internal usage types
    Co-authored-by: Enzo Ferey <hello@enzoferey.com>

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,3 @@
 export { getParsedEthersError } from "./getParsedEthersError";
 export { RETURN_VALUE_ERROR_CODES } from "./constants";
-export type { BigNumber } from "./types";
-export type { EthersError } from "./types";
-export type { NestedEthersError } from "./types";
-export type { ReturnValue } from "./types";
+export type { EthersError, ReturnValue } from "./types";
